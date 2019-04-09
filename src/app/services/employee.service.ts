f0d41455708325404ac4c4c0a292e8972479fedd
Rafael Paredes <Rafael.X.Paredes.-ND@disney.com>
2019-04-09 17:39:18 -0500

    Adding generics to employee service

--- src/app/services/employee.service.ts
+++ src/app/services/employee.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Employee } from '../interfaces/Employee';
 
 @Injectable({
   providedIn: 'root'
@@ -9,8 +11,8 @@ export class EmployeeService {
   constructor(public http: HttpClient) { }
 
 
-  public getEmployees() {
+  public getEmployees(): Observable<Employee> {
     const endpoint = 'http://localhost:8080/rest/employees/';
-    return this.http.get(endpoint);
+    return this.http.get<Employee>(endpoint);
   }
 }
